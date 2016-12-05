fd3d6449bedb8fbf134ae370c8b7be722b39a098
casp122b <casp122b@easv365.dk>
2016-12-05 13:11:29 +0100

    Større ændringer i layout
    Jeg slettede vores MVC pattern, da det genererede alt for meget bøvl, så
    nu er alle controllers, views og models samlet i GUI mappen.

---
+++ GamersMyTunes/src/GUI/PlayerUIController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Casper
+ */
+public class PlayerUIController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+    @FXML
+    public void btnPlaylistUINew(ActionEvent event) throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/PlaylistUI.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+}
+}
