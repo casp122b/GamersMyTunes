c90a781fc430a7cdcc31e32c6e33c368ab4cb49e
Jens Nissen <nissenjens@hotmail.com>
2016-12-09 01:28:25 +0100

    Lavet den sidste close kanp

--- GamersMyTunes/src/GUI/PlaylistUIController.java
+++ GamersMyTunes/src/GUI/PlaylistUIController.java
@@ -7,21 +7,34 @@ package GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 /**
  * FXML Controller class
  *
  * @author Casper & Jens
  */
 public class PlaylistUIController implements Initializable {
+    
+    @FXML
+    private Button btnCancel;
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+        @FXML
+    private void btnCancelActionPerformed(ActionEvent event) 
+    {
+        Stage stage = (Stage) btnCancel.getScene().getWindow();
+        stage.close();
+    }
 }
