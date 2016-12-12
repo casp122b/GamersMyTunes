ea43149b14f33795042a0693f198ac857e3b7141
casp122b <casp122b@easv365.dk>
2016-12-12 18:31:13 +0100

    Major layout update
    I changed the layout, so it fits, what we talked about at school today.
    I removed the edit buttons, up button, down button, previous button,
    next button and songs on playlist delete button. I also adjusted the
    windows, so it looks better overall.

--- GamersMyTunes/src/GUI/Controller/PlayerUIController.java
+++ GamersMyTunes/src/GUI/Controller/PlayerUIController.java
@@ -48,31 +48,15 @@ public class PlayerUIController implements Initializable {
     @FXML
     public AnchorPane root;
     @FXML
-    public Button btnPrev;
-    @FXML
     public Button btnPlay;
-    @FXML
-    public Button btnNext;
     @FXML
     public Button btnStop;
     @FXML
     public Button btnPlaylistNew;
     @FXML
-    public Button btnPlaylistEdit;
+    public Button btnTblSongsNew;
     @FXML
-    public Button btnPlaylistDel;
-    @FXML
-    public Button btnUp;
-    @FXML
-    public Button btnDown;
-    @FXML
-    public Button btnDel;
-    @FXML
-    public Button btnSongUINew;
-    @FXML
-    public Button btnSongUIEdit;
-    @FXML
-    public Button btnSongUIDel;
+    public Button btnTblSongsDelete;
     @FXML
     public TableView<Music> tblSOP;
     @FXML
@@ -107,11 +91,10 @@ public class PlayerUIController implements Initializable {
     public TableColumn<Music, String> SOPColTitle;
     @FXML
     public TableColumn<Music, String> SOPColArtist;
-    
+
     private Music selectedSong;
     private Playlist selectedPlaylist;
     private boolean isPlaying;
-    private boolean isMuted;
     private MusicManager musicManager;
     private final ObservableList<Music> songsLibrary;
     private ObservableList<Music> currentSongsInView;
@@ -119,68 +102,70 @@ public class PlayerUIController implements Initializable {
     private PlaylistManager playlistManager;
     private SongModel songModel;
     private PlaylistModel playlistModel;
-    
+    @FXML
+    private Button btnPlaylistDelete;
+    @FXML
+    private Button btnAddToPlaylist;
+    @FXML
+    private Label lblNowPlaying;
+
     /**
      * The default contructor for this class.
      */
-    public PlayerUIController()
-    {
+    public PlayerUIController() {
         this.songsLibrary = FXCollections.observableArrayList();
         this.currentSongsInView = FXCollections.observableArrayList();
         this.playlists = FXCollections.observableArrayList();
     }
-    
+
     /**
-     * This method initializes all its content, when PlayerUIController is launched.
+     * This method initializes all its content, when PlayerUIController is
+     * launched.
+     *
      * @param url
-     * @param rb 
+     * @param rb
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) 
-    {
-            songsColTitle.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getTitle()));
-            songsColArtist.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getArtist()));
-            songsColCategory.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getGenre()));
-            songsColTime.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getTime()));
-            playlistsColName.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getName()));
-            playlistsColSongs.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getSongs()));
-            SOPColTitle.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getTitle()));
-            SOPColArtist.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getArtist()));
+    public void initialize(URL url, ResourceBundle rb) {
+        songsColTitle.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getTitle()));
+        songsColArtist.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getArtist()));
+        songsColCategory.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getGenre()));
+        songsColTime.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getTime()));
+        playlistsColName.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getName()));
+        playlistsColSongs.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getSongs()));
+        SOPColTitle.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getTitle()));
+        SOPColArtist.setCellValueFactory(value -> new SimpleObjectProperty<>(value.getValue().getArtist()));
 
-            musicManager = new MusicManager();
-            playlistManager = new PlaylistManager();
-            songModel = SongModel.getInstance();
-            playlistModel = PlaylistModel.getInstance();
-            isPlaying = false;
-            tblSongs.setItems(songModel.getSongs());
-            tblPlaylists.setItems(playlistModel.getPlaylists());
-            tblSOP.setItems(playlistModel.getSongsOnPlaylist());
-            initialLoad();
-            setPlaylists();
-            currentSongsInView = songsLibrary;
-            searchOnUpdate();
+        musicManager = new MusicManager();
+        playlistManager = new PlaylistManager();
+        songModel = SongModel.getInstance();
+        playlistModel = PlaylistModel.getInstance();
+        isPlaying = false;
+        tblSongs.setItems(songModel.getSongs());
+        tblPlaylists.setItems(playlistModel.getPlaylists());
+        tblSOP.setItems(playlistModel.getSongsOnPlaylist());
+        initialLoad();
+        setPlaylists();
+        currentSongsInView = songsLibrary;
+        searchOnUpdate();
     }
+
     /**
      * @param event
      */
-    
-    private void initialLoad()
-    {
-        try
-        {
+
+    private void initialLoad() {
+        try {
             songModel.loadSongData();
             playlistModel.loadPlaylistData();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Logger.getLogger(PlayerUIController.class.getName()).log(Level.SEVERE, null, e);
         }
         tblSongs.setItems(songModel.getSongs());
     }
-    
+
     @FXML
-    public void btnPlaylistUINew(ActionEvent event) throws Exception 
-    {
+    public void btnPlaylistUINew(ActionEvent event) throws Exception {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/PlaylistUI.fxml"));
             Parent root = loader.load();
@@ -193,21 +178,101 @@ public class PlayerUIController implements Initializable {
             e.printStackTrace();
         }
     }
-    
-    public void addToSongs(Music m)
-    {
+
+    @FXML
+    public void handleOnMousePressed(MouseEvent event) {
+        selectedSong = tblSongs.selectionModelProperty().getValue().getSelectedItem();
+        if (event.isPrimaryButtonDown() && event.getClickCount() == 2) {
+            if (selectedSong != null) {
+                musicManager.pauseSong();
+                musicManager.playSong(selectedSong, true);
+                processMediaInfo();
+            }
+        }
+    }
+
+    @FXML
+    public void btnCloseActionPerformed(ActionEvent event) {
+        songModel.saveSongData();
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    public void btnPlayActionPerformed(ActionEvent event) {
+        // Making sure the song is never null before trying to play a song.
+        if (selectedSong == null) {
+            tblSongs.selectionModelProperty().get().select(0);
+        }
+
+        selectedSong = tblSongs.selectionModelProperty().getValue().getSelectedItem();
+        //Play button pressed
+        if (!isPlaying) {
+            musicManager.playSong(selectedSong, false);
+        } else {
+            musicManager.pauseSong();
+        }
+
+        processMediaInfo();
+    }
+
+    /**
+     * Deletes the selected song from tblSongs.
+     */
+    @FXML
+    public void btnTblSongsDeleteActionPerformed() {
+        deleteSong();
+
+    }
+
+    /**
+     * Deletes the selected playlist from tblPlaylists.
+     */
+    @FXML
+    public void btnPlaylistDeleteActionPerformed() {
+
+        deletePlaylist();
+
+    }
+
+    @FXML
+    public void btnAddToPlaylistActionPerformed(ActionEvent event) {
+        Music mIndex = tblSongs.selectionModelProperty().getValue().getSelectedItem();
+        Playlist pIndex = tblPlaylists.selectionModelProperty().getValue().getSelectedItem();
+        if (mIndex != null && pIndex != null) {
+            pIndex.addMusic(mIndex);
+
+            playlistModel.updatePlaylistView(pIndex.getMusiclist());
+        }
+    }
+
+    @FXML
+    public void tblChangePlaylistActionPerformed(MouseEvent event) {
+        Playlist pIndex = tblPlaylists.selectionModelProperty().getValue().getSelectedItem();
+        playlistModel.updatePlaylistView(pIndex.getMusiclist());
+
+    }
+
+    @FXML
+    public void btnStopActionPerformed(ActionEvent event) {
+        musicManager.stopSong();
+    }
+
+    @FXML
+    public void handleOnVolumeChanged() {
+        musicManager.getMediaPlayer().setVolume(sliderVolume.getValue() / 100);
+    }
+
+    public void addToSongs(Music m) {
         tblSongs.getItems().add(m);
     }
-    
-    public void addToPlaylist(Playlist pl)
-    {
+
+    public void addToPlaylist(Playlist pl) {
         tblPlaylists.getItems().add(pl);
-        
+
     }
-    
-    @FXML
-    public void btnSongUINew(ActionEvent event) throws Exception
-    {
+
+    public void btnTblSongsNewActionPerformed(ActionEvent event) throws Exception {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/SongUI.fxml"));
             Parent root = loader.load();
@@ -216,196 +281,81 @@ public class PlayerUIController implements Initializable {
             Stage stage = new Stage();
             stage.setScene(new Scene(root));
             stage.show();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    @FXML
-    public void handleOnMousePressed(MouseEvent event)
-    {
-        selectedSong = tblSongs.selectionModelProperty().getValue().getSelectedItem();
-        if (event.isPrimaryButtonDown() && event.getClickCount() == 2)
-        {
-            if (selectedSong != null)
-            {
-                musicManager.pauseSong();
-                musicManager.playSong(selectedSong, true);
-                processMediaInfo();
+
+    /**
+     * If there is a song currently playing, it will be stopped, and the
+     * selected song is deleted from tblSongs.
+     */
+    private void deleteSong() {
+        for (Music m : tblSongs.getItems()) {
+            if (m.getTitle() == selectedSong.getTitle()) {
+                tblSongs.getItems().remove(selectedSong);
+                musicManager.stopSong();
             }
         }
     }
-    
-    @FXML
-    public void btnCloseActionPerformed(ActionEvent event) 
-    {
-        songModel.saveSongData();
-        Stage stage = (Stage) btnClose.getScene().getWindow();
-        stage.close();
-    }
-    
-    @FXML
-    public void btnPlayActionPerformed(ActionEvent event)
-    {
-        // Making sure the song is never null before trying to play a song.
-        if (selectedSong == null)
-        {
-            tblSongs.selectionModelProperty().get().select(0);
-        }
-        
-        selectedSong = tblSongs.selectionModelProperty().getValue().getSelectedItem();
-        //Play button pressed
-        if (!isPlaying)
-        {
-            musicManager.playSong(selectedSong, false);
-        }
-        else
-        {
-            musicManager.pauseSong();
-        }
 
-        processMediaInfo();
-    }
-    
-    /**
-     *Deletes the selected song from tblSongs.
-     */
-    @FXML
-    public void btnSongUIDelActionPerformed()
-    {
-        deleteSong();
-
-    }
-    
-    /**
-     * If there is a song currently playing, it will be stopped,
-     * and the selected song is deleted from tblSongs.
-     */
-    private void deleteSong()
-    {
-        musicManager.stopSong();
-        songModel.getSongs().remove(selectedSong);
-        tblSongs.getItems().remove(selectedSong);
-    }
-    
-    /**
-     * Deletes the selected playlist from tblPlaylists.
-     */
-    @FXML
-    public void btnPlaylistDelActionPerformed()
-    {
-
-        deletePlaylist();
-
-    }
-    
-    private void deletePlaylist()
-    {
-        playlistModel.getInstance().getPlaylists().remove(selectedPlaylist);
-        tblPlaylists.getItems().remove(selectedPlaylist);
-
-    }
-    
-    @FXML
-    public void btnAddToPlaylistActionPerformed(ActionEvent event)
-    {
-        Music mIndex = tblSongs.selectionModelProperty().getValue().getSelectedItem();
-        Playlist pIndex = tblPlaylists.selectionModelProperty().getValue().getSelectedItem();
-        if (mIndex != null && pIndex != null)
-        {
-            pIndex.addMusic(mIndex);
-
-            playlistModel.updatePlaylistView(pIndex.getMusiclist());
+    private void deletePlaylist() {
+        for (Playlist pl : tblPlaylists.getItems()) {
+            if (pl.getName() == selectedPlaylist.getName()) {
+                tblPlaylists.getItems().remove(selectedPlaylist);
+            }
         }
     }
-    
-    public void setSongs()
-    {
+
+    public void setSongs() {
         songsLibrary.addAll(songModel.getSongs());
     }
-    
-    public void setPlaylists()
-    {
+
+    public void setPlaylists() {
         playlists = playlistModel.getPlaylists();
     }
-    
-    @FXML
-    public void tblChangePlaylistActionPerformed(MouseEvent event)
-    {
-        Playlist pIndex = tblPlaylists.selectionModelProperty().getValue().getSelectedItem();
-        playlistModel.updatePlaylistView(pIndex.getMusiclist());
-        
-    }
-    
-    @FXML
-    public void btnStopActionPerformed(ActionEvent event)
-    {
-        musicManager.stopSong();
-    }
-    
-    @FXML
-    public void handleOnVolumeChanged()
-    {
-    musicManager.getMediaPlayer().setVolume(sliderVolume.getValue() / 100);
-    }
-    
-    private void searchOnUpdate()
-    {
+
+    private void searchOnUpdate() {
         txtFilter.textProperty().addListener((listener, oldVal, newVal)
-                -> 
-                {
-                    ObservableList<Music> searchedSongs = FXCollections.observableArrayList();
-                    searchedSongs.clear();
-                    ObservableList<Music> allSongsInCurrentView = currentSongsInView;
-                    if (selectedPlaylist == null)
-                    {
-                        allSongsInCurrentView.setAll(songModel.getSongs());
-                    }
-                    else
-                    {
-                        allSongsInCurrentView.setAll(selectedPlaylist.getSongList());
-                    }
+                -> {
+            ObservableList<Music> searchedSongs = FXCollections.observableArrayList();
+            searchedSongs.clear();
+            ObservableList<Music> allSongsInCurrentView = currentSongsInView;
+            if (selectedPlaylist == null) {
+                allSongsInCurrentView.setAll(songModel.getSongs());
+            } else {
+                allSongsInCurrentView.setAll(selectedPlaylist.getSongList());
+            }
 
-                    for (Music m : allSongsInCurrentView)
-                    {
-                        if (m.getTitle().trim().toLowerCase().contains(newVal.trim().toLowerCase())
-                                || m.getArtist().trim().toLowerCase().contains(newVal.trim().toLowerCase())
-                                || m.getGenre().trim().toLowerCase().contains(newVal.trim().toLowerCase())
-                                || String.valueOf(m.getRating()).trim().toLowerCase().contains(newVal.trim().toLowerCase())
-                                && !searchedSongs.contains(m))
-                        {
-                            searchedSongs.add(m);
-                        }
-                    }
+            for (Music m : allSongsInCurrentView) {
+                if (m.getTitle().trim().toLowerCase().contains(newVal.trim().toLowerCase())
+                        || m.getArtist().trim().toLowerCase().contains(newVal.trim().toLowerCase())
+                        || m.getGenre().trim().toLowerCase().contains(newVal.trim().toLowerCase())
+                        || String.valueOf(m.getRating()).trim().toLowerCase().contains(newVal.trim().toLowerCase())
+                        && !searchedSongs.contains(m)) {
+                    searchedSongs.add(m);
+                }
+            }
 
-                    tblSongs.setItems(searchedSongs);
+            tblSongs.setItems(searchedSongs);
         });
     }
 
-    private void processMediaInfo()
-    {
-        try
-        {
+    private void processMediaInfo() {
+        try {
             musicManager.getMediaPlayer().currentTimeProperty().addListener(new ChangeListener<Duration>() {
                 @Override
                 public void changed(ObservableValue<? extends Duration> listener, Duration oldVal, Duration newVal) {
                     ;
                 }
             });
-            if (!isPlaying)
-            {
+            if (!isPlaying) {
+                lblNP.setText(musicManager.getCurrentlyPlayingSong().getTitle());
+            } else {
                 lblNP.setText(musicManager.getCurrentlyPlayingSong().getTitle());
             }
-            else
-            {
-                lblNP.setText(musicManager.getCurrentlyPlayingSong().getTitle());
-            }
-        }
-        finally
-        {
+        } finally {
 
         }
-}
+    }
 }
